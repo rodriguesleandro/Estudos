bac5355661cb53473b382620d015df226970239d
Leandro da Silva Rodrigues <leandror@praiagrande.sp.gov.br>
2021-04-13 16:14:54 -0300

    Angular continuacao

--- core/aspNet/aspNetWebAPIFullStack/ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ core/aspNet/aspNetWebAPIFullStack/ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  isImagemExibida: boolean = true;
+
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+  public set filtroLista(value:string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this._filtroLista): this.eventos;
+  }
+
+  public filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (ev: { tema: string, local:string; }) => ev.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      || ev.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+
+    );
+  }
+
+  private _filtroLista: string = '';
+
 
   constructor(private http: HttpClient) { }
 
@@ -16,10 +40,18 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  exibirImagem(){
+    this.isImagemExibida = !this.isImagemExibida
+  }
+
   public getEventos(): void{
     console.log('Puta que pariu');
     this.http.get('https://localhost:5001/api/Eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+
+      },
       error => console.log(error)
     );
 

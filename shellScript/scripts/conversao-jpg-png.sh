69bf14ce1a18d9ea84c816a107526d9d1f385499
leandro <rodrigues.leandro@gmail.com>
2022-02-25 17:04:28 -0300

    altera o hardcode para receber parametro de entrada

--- shellScript/scripts/conversao-jpg-png.sh
+++ shellScript/scripts/conversao-jpg-png.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-convert ~/Downloads/imagens-livros/amazon_aws.jpg ~/Downloads/imagens-livros/amazon_aws.png
+convert ~/Downloads/imagens-livros/$1.jpg ~/Downloads/imagens-livros/$1.png

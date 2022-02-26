d7559559fe4c5fc00b892468e0162168621abd9e
leandro <rodrigues.leandro@gmail.com>
2022-02-26 13:01:14 -0300

    Verifica o status da saida
    - Encapsula o codigo em uma funcao
    - Delimita o escopo da variavel para local
    - Valida o status da saida e exibe msg amigavel
    - Direciona erro para arquivo especifico

--- shellScript/scripts/conversao-jpg-png.sh
+++ shellScript/scripts/conversao-jpg-png.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-cd ~/Downloads/imagens-livros
+converte_imagens(){
+    cd ~/Downloads/imagens-livros
 
-if [ ! -d png ]
+    if [ ! -d png ]
+    then
+        mkdir png
+    fi
+
+    for imagem in *.jpg
+    do
+        local imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+        convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
+    done
+}
+
+converte_imagens 2>erro.txt
+
+if [ $? -eq 0 ]
 then
-    mkdir png
+    echo "Conversao realizada com sucesso"
+else
+    echo "Houve Falha no processo de conversao"
 fi
-
-for imagem in *.jpg
-do
-    imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
-    convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
-done
